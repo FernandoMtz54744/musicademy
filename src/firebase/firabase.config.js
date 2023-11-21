6a287663e3abf0c996a0d678c9d8d0be410814ab
Fernando <fer_f@outlook.com>
2023-11-20 21:14:16 -0600

    Modificación de acceso a las credenciales de la api firebase

--- src/firebase/firabase.config.js
+++ src/firebase/firabase.config.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsdvIZig20qp_aKImce8HvSjFFXAcEFKQ",
-  authDomain: "musicademy-aaa17.firebaseapp.com",
-  projectId: "musicademy-aaa17",
-  storageBucket: "musicademy-aaa17.appspot.com",
-  messagingSenderId: "75631616180",
-  appId: "1:75631616180:web:92e0f647203524054a77e1"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
+console.log(process.env.REACT_APP_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
