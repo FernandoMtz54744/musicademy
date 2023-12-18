3a0058f7a12091474247c8d0550e2259abefa25f
Fernando <fer_f@outlook.com>
2023-12-17 22:14:49 -0600

    Responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ import manual from "../res/manuales/Login.pdf";
 
 export default function Login({data, handleChange, handleLogin, handleLoginGoogle, error}) {
   return (
-    <div>
+    <>
       <header className='header header-black'>
         <img src={logo} className="logo" alt='logo'/>
         <div className='header-links'>
@@ -58,6 +58,6 @@ export default function Login({data, handleChange, handleLogin, handleLoginGoogl
           <path className='waves-cover waves-black'  d="M0,224L40,213.3C80,203,160,181,240,181.3C320,181,400,203,480,224C560,245,640,267,720,261.3C800,256,880,224,960,224C1040,224,1120,256,1200,266.7C1280,277,1360,267,1400,261.3L1440,256L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"/>
         </svg>
       </footer>
-    </div>
+    </>
   )
 }
